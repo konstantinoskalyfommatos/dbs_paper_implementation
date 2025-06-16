24ea20267b0be7cc8e7282ee4f042aa00b44c212
giorgospets <gmpetsangourakis@gmail.com>
2025-06-16 12:51:21 +0300

    plots

--- src/pipelines/data_preprocessing_pipeline.sh
+++ src/pipelines/data_preprocessing_pipeline.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-root_dir=curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../" && pwd )"
 
 cd ${root_dir}
-
-mkdir ./data/
-
-mkdir ./models/
 
 python ./src/data_processing/download_data.py
 
